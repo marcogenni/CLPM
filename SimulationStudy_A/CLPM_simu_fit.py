5608182227508dc16f5dc8d03eaa174aa3c660f5
mcorneli <marcogenni@gmail.com>
2021-01-04 16:54:24 +0100

    last changes

--- SimulationStudy_A/CLPM_simu_fit.py
+++ SimulationStudy_A/CLPM_simu_fit.py
@@ -11,10 +11,12 @@ import torch
 
 import sys
 sys.path.append('../')
-from CLPM_fit import MDataset, FitOneShot
-from CLPM_plot import make_video
+from CLPM_fit import *
+from CLPM_plot import *
 
 device = "cpu"
+
+folder = ''
 
 
 #### DATA AND PARAMETER INITIALISATION
@@ -75,34 +77,67 @@ pd.DataFrame(loss_function_values).to_csv(path+'output/loss_function_values.csv'
  ## Plot results ##
 ####################
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-folder = ''
+# folder = ''
 
-Z_long = np.genfromtxt(folder+'output/positions.csv', delimiter = ",")
-n_nodes = np.max(Z_long[:,0]).astype(int)
-n_dimensions = np.max(Z_long[:,1]).astype(int)
-n_time_frames = np.max(Z_long[:,2]).astype(int)
-Z = np.zeros((n_nodes, n_dimensions, n_time_frames))
-for index in range(len(Z_long)):
-    i_ = (Z_long[index,0]-1).astype(int)
-    j_ = (Z_long[index,1]-1).astype(int)
-    k_ = (Z_long[index,2]-1).astype(int)
-    Z[i_,j_,k_] = Z_long[index,3]
+# Z_long = np.genfromtxt(folder+'output/positions.csv', delimiter = ",")
+# n_nodes = np.max(Z_long[:,0]).astype(int)
+# n_dimensions = np.max(Z_long[:,1]).astype(int)
+# n_time_frames = np.max(Z_long[:,2]).astype(int)
+# Z = np.zeros((n_nodes, n_dimensions, n_time_frames))
+# for index in range(len(Z_long)):
+#     i_ = (Z_long[index,0]-1).astype(int)
+#     j_ = (Z_long[index,1]-1).astype(int)
+#     k_ = (Z_long[index,2]-1).astype(int)
+#     Z[i_,j_,k_] = Z_long[index,3]
     
-for snap in range(Z.shape[2]):
-        plt.figure("Latent Positions")
-        plt.xlim((-3.0,3.0))
-        plt.ylim((-3.0,3.0))
-        for idi in range(n_nodes):    
-            plt.plot(Z[idi,0,snap], Z[idi,1,snap], 'ro')
-        plt.savefig(folder+'results/snaps/snap_'+str(snap)+'.png', dpi = 200)
-        plt.close()
+# for snap in range(Z.shape[2]):
+#         plt.figure("Latent Positions")
+#         plt.xlim((-3.0,3.0))
+#         plt.ylim((-3.0,3.0))
+#         for idi in range(n_nodes):    
+#             plt.plot(Z[idi,0,snap], Z[idi,1,snap], 'ro')
+#         plt.savefig(folder+'results/snaps/snap_'+str(snap)+'.png', dpi = 200)
+#         plt.close()
         
-img=[]
-for i in range(n_time_frames):
-    img.append(folder+'results/snaps/snap_'+str(i)+'.png')
+# img=[]
+# for i in range(n_time_frames):
+#     img.append(folder+'results/snaps/snap_'+str(i)+'.png')
     
-test = make_video(folder+'results/video.mp4', images = img)
+# test = make_video(folder+'results/video.mp4', images = img)
    
+
+### PLOTS
+
+outvid = folder + 'results/video.mp4'
+frames_btw = 20
+node_colors = fade_node_colors(dataset, Z, bending = 1)
+node_sizes = fade_node_sizes(dataset, bending = 1)
+dpi = 100
+period = 1
+size = (1200,900)
+is_color = True
+formato = 'mp4v'
+
+clpm_animation(outvid, Z.detach().numpy(), changepoints.detach().numpy(), frames_btw, node_colors, node_sizes, dpi, period, size, is_color, formato)
+
+#plt.plot(loss_function_values)
+
+
+### EXPORT OUTPUT
+
+Z_long_format = np.zeros([Z.shape[0]*Z.shape[1]*Z.shape[2], 4])# just writing the contents of this array in a long format so that I can read it with R
+index = 0
+for i in range(Z.shape[0]):
+	for d in range(Z.shape[1]):
+		for t in range(Z.shape[2]):
+			Z_long_format[index, 0] = i+1
+			Z_long_format[index, 1] = d+1
+			Z_long_format[index, 2] = t+1
+			Z_long_format[index, 3] = Z[i,d,t]
+			index += 1
+
+pd.DataFrame(Z_long_format).to_csv(folder+'output/positions.csv', index = False, header = False)
+pd.DataFrame(loss_function_values).to_csv(folder+'output/loss_function_values.csv', index = False, header = False)
 
